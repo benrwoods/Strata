7525935d69fde281c5610857f3681ae1dd89d757
Ben Woods <ben.woods@barnett-waddingham.co.uk>
2018-01-21 19:16:36 +0000

    Fix

--- Strata/Model/Shopping/ItemDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Strata.API.Model.Shopping {
-    public class ItemDTO {
-        public string Name { get; set; }
-
-        public int Quantity { get; set; }
-
-        public double Cost { get; set; }
-    }
-}
--- Strata/Model/Shopping/OrderListDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Strata.API.Model.Shopping {
-    public class OrderListDTO {
-
-        public int Id { get; set; }
-
-        public double Cost { get; set; }
-
-    }
-}
